235a88e21f576408d29cf396af732ca38bd14ea3
IldarDav <134400170+IldarDav@users.noreply.github.com>
2023-07-04 17:07:05 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "6367492326:AAFb0Qd_7Wlz7ZtWtHJHtP_83bGY9HJ7k7o"
+
+keys = {
+    'Евро': 'EUR',
+    'евро': 'EUR',
+    'Доллар': 'USD',
+    'доллар': 'USD',
+    'Рубль': 'RUB',
+    'рубль': 'RUB',
+}
